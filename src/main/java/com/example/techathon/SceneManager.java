badabb7321e7714597e27cc8723a9b8e50c95321
charlie knox <charlieknox704@gmail.com>
2025-01-15 10:36:24 +0000

    scene manager

---
+++ src/main/java/com/example/techathon/SceneManager.java
@@ -0,0 +1,20 @@
+package com.example.techathon;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.util.HashMap;
+
+public class SceneManager {
+    private Stage stage;
+    private HashMap<String, Scene> sceneHashMap;
+
+    public SceneManager(Stage stage){
+        this.stage = stage;
+        this.sceneHashMap = new HashMap<>();
+    }
+
+    public void addScene(Scene scene, String route){
+        this.sceneHashMap.put(route, scene);
+    }
+
+}
