42de8184671c9e87ac28df60a512b3cdcc096044
horiaomar25 <horiaomar6@gmail.com>
2025-01-16 14:28:06 +0000

    article navigation fix

---
+++ src/main/java/com/example/techathon/ArticleController.java
@@ -0,0 +1,35 @@
+package com.example.techathon;
+
+import javafx.fxml.FXML;
+
+public class ArticleController {
+    private SceneManager sceneManager;
+    private NavController navController;
+
+    public void setSceneManager(SceneManager sceneManager){
+        this.sceneManager = sceneManager;
+    }
+    public void setNavContoller(NavController controller){
+        this.navController = controller;
+    }
+    @FXML
+    protected void onHelloButtonClick() {
+        this.navController.onHomeButtonClick();
+    }
+    @FXML
+    protected void onLearnButtonClick() {
+        this.navController.onLearnButtonClick();
+    }
+    @FXML
+    protected void onQuizButtonClick() {
+        this.navController.onQuizButtonClick();
+    }
+    @FXML
+    protected void onCalculatorButtonClick() {
+        this.navController.onCalculatorButtonClick();
+    }
+    @FXML
+    protected void onNotesButtonClick() {
+        this.navController.onNotesButtonClick();
+    }
+}
