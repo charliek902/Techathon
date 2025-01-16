0641e48b9067ec77422274e0dfd964881e50d06c
TheBestMario <mariogeorgiev02@gmail.com>
2025-01-16 16:41:29 +0000

    added budget-calc functionality

--- src/main/java/com/example/techathon/CalculatorController.java
+++ src/main/java/com/example/techathon/CalculatorController.java
@@ -6,6 +6,8 @@ import javafx.scene.control.*;
 import java.io.IOException;
 
 public class CalculatorController {
+
+    //>ISA view components
     @FXML
     private TextField amountField;
     @FXML
@@ -13,9 +15,70 @@ public class CalculatorController {
     @FXML
     private Label ISAOutputLabel;
 
+    //>Budgeting view components
+    //>>Income
+    @FXML
+    private TextField salaryField;
+    @FXML
+    private TextField rentField;
+    @FXML
+    private TextField pensionsField;
+    @FXML
+    private TextField investmentsField;
+    @FXML
+    private TextField universalCreditField;
+    @FXML
+    private TextField additionalIncomeField;
+    //>>Bills
+    @FXML
+    private TextField mortgageCostField;
+    @FXML
+    private TextField rentCostField;
+    @FXML
+    private TextField utilitiesCostField;
+    @FXML
+    private TextField lifeInsuranceCostField;
+    @FXML
+    private TextField councilTaxField;
+    @FXML
+    private TextField broadbandCostField;
+    //>>Living Costs
+    @FXML
+    private TextField groceriesCostField;
+    @FXML
+    private TextField deliveryCostField;
+    @FXML
+    private TextField alcoholCostField;
+    @FXML
+    private TextField travelCostField;
+    @FXML
+    private TextField clothingCostField;
+    @FXML
+    private TextField toiletriesCostField;
+    //>>Leisure
+    @FXML
+    private TextField daysOutCostField;
+    @FXML
+    private TextField subscriptionsCostField;
+    @FXML
+    private TextField hobbiesCostField;
+    @FXML
+    private TextField celebrationsCostField;
+    @FXML
+    private TextField holidaysCostField;
+    //>>result and summary
+    @FXML
+    private Label totalIncomeLabel;
+    @FXML
+    private Label totalCostLabel;
+    @FXML
+    private Label budgetResultLabel;
+
+
+
     private NavController navController;
     private SceneManager sceneManager;
-    private ToggleGroup group;
+    private ToggleGroup radioGroup;
 
     @FXML
     private RadioButton growthISACheckBoxOption1;
@@ -29,10 +92,10 @@ public class CalculatorController {
     @FXML
     public void initialize() {
         // Grouping the radio buttons
-        group = new ToggleGroup();
-//        growthISACheckBoxOption1.setToggleGroup(group);
-//        growthISACheckBoxOption2.setToggleGroup(group);
-//        growthISACheckBoxOption3.setToggleGroup(group);
+        radioGroup = new ToggleGroup();
+//        growthISACheckBoxOption1.setToggleGroup(radioGroup);
+//        growthISACheckBoxOption2.setToggleGroup(radioGroup);
+//        growthISACheckBoxOption3.setToggleGroup(radioGroup);
     }
     public void setNavContoller(NavController navController) {
         this.navController = navController;
@@ -43,13 +106,14 @@ public class CalculatorController {
 
 
 
+
     @FXML
     private void handleISACalculate() {
         double amount = Double.parseDouble(amountField.getText());
         short time = Short.parseShort(timeField.getText());
 
         // Determine the ISAInterest based on the selected radio button
-        RadioButton selectedRadioButton = (RadioButton) group.getSelectedToggle();
+        RadioButton selectedRadioButton = (RadioButton) radioGroup.getSelectedToggle();
         if (selectedRadioButton == growthISACheckBoxOption1) {
             ISAInterest = 2;
         } else if (selectedRadioButton == growthISACheckBoxOption2) {
@@ -73,6 +137,56 @@ public class CalculatorController {
         double amount = Double.parseDouble(amountField.getText());
         short time = Short.parseShort(timeField.getText());
         double result = time*amount;
+    }
+
+    @FXML
+    private void handleBudgetCalculate() {
+        double income = 0;
+        double expenses = 0;
+        try {
+            // Calculate total income
+            income += parseDoubleNullIsZero(salaryField.getText());
+            income += parseDoubleNullIsZero(rentField.getText());
+            income += parseDoubleNullIsZero(pensionsField.getText());
+            income += parseDoubleNullIsZero(investmentsField.getText());
+            income += parseDoubleNullIsZero(universalCreditField.getText());
+            income += parseDoubleNullIsZero(additionalIncomeField.getText());
+
+            // Calculate total expenses
+            expenses += parseDoubleNullIsZero(mortgageCostField.getText());
+            expenses += parseDoubleNullIsZero(rentCostField.getText());
+            expenses += parseDoubleNullIsZero(utilitiesCostField.getText());
+            expenses += parseDoubleNullIsZero(lifeInsuranceCostField.getText());
+            expenses += parseDoubleNullIsZero(councilTaxField.getText());
+            expenses += parseDoubleNullIsZero(broadbandCostField.getText());
+            expenses += parseDoubleNullIsZero(groceriesCostField.getText());
+            expenses += parseDoubleNullIsZero(deliveryCostField.getText());
+            expenses += parseDoubleNullIsZero(alcoholCostField.getText());
+            expenses += parseDoubleNullIsZero(travelCostField.getText());
+            expenses += parseDoubleNullIsZero(clothingCostField.getText());
+            expenses += parseDoubleNullIsZero(toiletriesCostField.getText());
+            expenses += parseDoubleNullIsZero(daysOutCostField.getText());
+            expenses += parseDoubleNullIsZero(subscriptionsCostField.getText());
+            expenses += parseDoubleNullIsZero(hobbiesCostField.getText());
+            expenses += parseDoubleNullIsZero(celebrationsCostField.getText());
+            expenses += parseDoubleNullIsZero(holidaysCostField.getText());
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+        }
+
+        totalIncomeLabel.setText("£" + income);
+        totalCostLabel.setText("£" + expenses);
+
+        double remainingBudget = income - expenses;
+        budgetResultLabel.setText("£" + remainingBudget);
+    }
+
+    //Custom parsing method for text fields when they are null to make them 0.
+    private double parseDoubleNullIsZero(String text) {
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
+        return Double.parseDouble(text);
     }
     @FXML
     protected void onHelloButtonClick() {
