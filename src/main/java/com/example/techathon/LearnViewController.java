596285974ca7cff2d0971a50f3a5bdadcbbda4b6
horiaomar25 <horiaomar6@gmail.com>
2025-01-16 13:38:10 +0000

    added: article1 linked

--- src/main/java/com/example/techathon/LearnViewController.java
+++ src/main/java/com/example/techathon/LearnViewController.java
@@ -1,6 +1,13 @@
 package com.example.techathon;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LearnViewController {
     private SceneManager sceneManager;
@@ -31,5 +38,20 @@ public class LearnViewController {
     @FXML
     protected void onNotesButtonClick() {
         this.navController.onNotesButtonClick();
+    }
+
+    @FXML
+    public void goToArticle1(MouseEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/techathon/Article1.fxml"));
+        Scene scene = new Scene(loader.load(), 600, 600);
+
+
+        scene.getStylesheets().add(getClass().getResource("/com/example/techathon/app.css").toExternalForm());
+        // Node used to handle mouse action and targets the VBox that is being clicked and activates the scene
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Boost Your Money");
+        stage.show();
+
     }
 }
